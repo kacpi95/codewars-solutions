aadff78e4e7c490f923b93db8de4e923a88b87ac
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-03-11 11:06:54 +0100

    Add task 7

--- js/script.js
+++ js/script.js
@@ -78,14 +78,26 @@
 // Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
 // For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
-const quarterOf = (month) => {
-	if (month >= 1 && month <= 3) {
-		return 1;
-	} else if (month >= 4 && month <= 6) {
-		return 2;
-	} else if (month >= 7 && month <= 9) {
-		return 3;
-	} else if (month >= 10 && month <= 12) {
-		return 4;
+// const quarterOf = (month) => {
+// 	if (month >= 1 && month <= 3) {
+// 		return 1;
+// 	} else if (month >= 4 && month <= 6) {
+// 		return 2;
+// 	} else if (month >= 7 && month <= 9) {
+// 		return 3;
+// 	} else if (month >= 10 && month <= 12) {
+// 		return 4;
+// 	}
+// };
+//                      Zadanie 7
+// Even or Odd Check
+// Create a function that takes a number as input and checks whether the number is even or odd. Return the string "even number" for even numbers and "odd number" for odd numbers.
+
+const isEven = (num) => {
+	if (num % 2 === 0) {
+		return "even number";
+	} else {
+		return "odd number";
 	}
 };
+isEven();
